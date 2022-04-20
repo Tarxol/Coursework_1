35c44f58f548105a7c48c04309c42a26d6e34c83
Tarxol <tarxol@yandex.ru>
2022-04-20 18:15:48 +0300

    2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-import os
 import requests
 from datetime import datetime
 import json
-import shutil
 from tqdm import tqdm
 
 token_vk = 'a67f00c673c3d4b12800dd0ba29579ec56d804f3c5f3bbcef5328d4b3981fa5987b951cf2c8d8b24b9abd'
 version = '5.131'
 files_list = []
+files_list_url = {}
 
 class VkUser:
 
@@ -28,53 +27,50 @@ class VkUser:
             'photo_sizes': 1
         }
         photo_lists = requests.get(photo_url, params={**self.params, **photo_get_params}).json()
+        a = input(f'\nВведите кол-во скачиваемых фото(по умолчанию 5): ')
+        if a.isnumeric() == False:
+            quantity_photo = 5
+        else:
+            quantity_photo = int(a)
         photo_lists_info = []
-        os.mkdir(put_to_dyrectory)
-        for photo in photo_lists['response']['items']:
+        for photo in photo_lists['response']['items'][:quantity_photo]:
             info = {'file_name': str(photo['likes']['count']) +'.jpg',
                     'size': photo['sizes'][-1]['type']}
             info_1 = {'file_name': str(photo['likes']['count']) + '_' + str(datetime.fromtimestamp(photo['date']).date()) + '.jpg',
                     'size': photo['sizes'][-1]['type']}
             if info['file_name'] in files_list:
-                image_bin = requests.get(photo['sizes'][-1]['url'])
-                open(f"{put_to_dyrectory}/{info_1['file_name']}", 'wb').write(image_bin.content)
+                files_list_url[info_1['file_name']] = photo['sizes'][-1]['url']
                 photo_lists_info.append(info_1)
                 files_list.append(info_1['file_name'])
             else:
-                image_bin = requests.get(photo['sizes'][-1]['url'])
-                open(f'{put_to_dyrectory}/{info["file_name"]}', 'wb').write(image_bin.content)
+                files_list_url[info['file_name']] = photo['sizes'][-1]['url']
                 photo_lists_info.append(info)
                 files_list.append(info['file_name'])
 
-        with open(f'{put_to_dyrectory}/data.json', "w") as write_file:
+        with open('data.json', "w") as write_file:
             json.dump(photo_lists_info, write_file)
-            files_list.append('data.json')
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token_ya
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
 
     def upload(self):
         for file in tqdm(files_list):
             upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-            headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'OAuth {}'.format(self.token)
-            }
-            params = {"path": put_to_dyrectory + '/' + file, "overwrite": "true"}
-            response = requests.get(upload_url, headers=headers, params=params)
-            href = response.json()['href']
-            response = requests.put(href, data=open(put_to_dyrectory + '/' + file, 'rb'))
-            response.raise_for_status()
+            params = {"path": put_to_dyrectory + '/' + file, "url": files_list_url[file]}
+            response = requests.post(upload_url, params=params, headers=self.headers)
+            if response.status_code != 202:
+                print(f'\nОшибка {response.status_code}')
+                quit()
+
 
     def create_folder(self, path_to_directory):
         url = "https://cloud-api.yandex.net/v1/disk/resources"
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token)
-        }
-        requests.put(f'{url}?path={path_to_directory}', headers=headers)
-
+        requests.put(f'{url}?path={path_to_directory}', headers=self.headers)
 
 if __name__ == '__main__':
 
@@ -86,5 +82,4 @@ if __name__ == '__main__':
     print()
     uploader.create_folder(put_to_dyrectory)
     result = uploader.upload()
-    shutil.rmtree(put_to_dyrectory, ignore_errors=False)
     print(f'\nКопирование файлов в папку: {put_to_dyrectory} , на Яндекс-Диск завершено.')
